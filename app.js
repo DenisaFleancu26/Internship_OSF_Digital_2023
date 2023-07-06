b8fa6a1fe7d4073bdf9c5cc9caf7a476088581de
Denisaa <101568110+DenisaFleancu26@users.noreply.github.com>
2023-07-06 20:14:06 +0300

    Configured Express & MongoDB

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var womensRouter = require('./routes/womens');
+var mensRouter = require('./routes/mens');
 
 var app = express();
-
+var hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+ 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/womens', womensRouter);
+app.use('/mens', mensRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +39,14 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/shop');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log("Connected to MongoDB");
 });
 
 module.exports = app;
