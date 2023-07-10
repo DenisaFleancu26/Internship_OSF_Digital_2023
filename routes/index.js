a61cf86b5bad60d70af05211132dac4f66946252
Denisaa <101568110+DenisaFleancu26@users.noreply.github.com>
2023-07-10 16:35:52 +0300

    Fectched products data from mongoDB

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,41 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { layout: 'layout', categories: null});
+  res.render('contentMainPage', { layout: 'layout', categories: null});
 });
 
-router.get('/:categories', async function(req, res, next) {
+/* GET category page */
+router.get('/:category', async function(req, res, next) {
   
   const retrieveDocument = async () => {
     var mongoose = require('mongoose')
     mongoose.connect('mongodb://127.0.0.1:27017/shop');
     const CategoryModel = require('../models/category');
-    return await CategoryModel.findOne({id: req.params.categories});
+    return await CategoryModel.findOne({id: req.params.category});
   }
   const category = await retrieveDocument();
 
-  switch(req.params.categories){
-    case 'mens':   
-        res.locals.isMensPage = true;
-        res.locals.isWomensPage = false;
-        break;
-    case 'womens':
-        res.locals.isMensPage = false;
-        res.locals.isWomensPage = true;
-        break;
-  }
+  res.locals.isCategoryPage = req.params.category;
 
-  res.render('index', { layout: 'layout', category: category, isMensPage: res.locals.isMensPage, isWomensPage: res.locals.isWomensPage});
+  res.render('contentMainPage', { layout: 'layout', category: category, isCategoryPage: res.locals.isCategoryPage});
+});
+
+/* GET subcategory page */
+router.get('/:category/:subcategory', async function(req, res, next){
+
+  const fetchProducts = async () => {
+    var mongoose = require('mongoose')
+    mongoose.connect('mongodb://127.0.0.1:27017/shop');
+    const ProductModel = require('../models/product');
+    return await ProductModel.find({primary_category_id: req.params.subcategory});
+  }
+  const products = await fetchProducts();
+
+  res.locals.isSubcategoryPage = req.params.subcategory;
+  res.locals.isCategoryPage = req.params.category;
+
+  res.render('contentSubcategoryPage', { layout: 'layout', products: products, isCategoryPage: res.locals.isCategoryPage, isSubcategoryPage: res.locals.isSubcategoryPage});
+
 });
 
 module.exports = router;
