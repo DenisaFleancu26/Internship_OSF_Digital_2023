43798a350ff13a2f31fdc4c7b70e5749e95fba7f
Denisaa <101568110+DenisaFleancu26@users.noreply.github.com>
2023-07-13 15:44:45 +0300

    Fetched product from MongoDB

--- routes/product.js
+++ routes/product.js
@@ -4,8 +4,27 @@ var router = express.Router();
 /* GET product detail page */
 router.get('/:category/:subcategory/:product', async function(req, res, next) {
   
-    res.render('contentProductDetailPage', { layout: 'layout' });
-    
-  });
+  const ProductModel = require('../models/product');
+  const product = await ProductModel.findOne({id: req.params.product});
+
+  if(!product){
+
+    next({ status: 404, message: 'Oops, This Page Not Found!'});
+
+  }else{
+
+    res.locals.isSubcategoryPage = req.params.subcategory;
+    res.locals.isCategoryPage = req.params.category;
+
+    if(product.short_description === product.long_description){
+      longDescription = false;
+    }else{
+      longDescription = true;
+    }
+
+    res.render('contentProductDetailPage', { layout: 'layout' , product: product, isCategoryPage: res.locals.isCategoryPage, isSubcategoryPage: res.locals.isSubcategoryPage, longDescription: longDescription});
+  }
+
+});
 
 module.exports = router;
