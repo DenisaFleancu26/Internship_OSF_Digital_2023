b76207f7e543378fa612d1b5c7ec3f427a8ec3eb
Denisaa <101568110+DenisaFleancu26@users.noreply.github.com>
2023-07-15 15:52:04 +0300

    Small change

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var categoryRouter = require('./routes/category');
 var subcategoryRouter = require('./routes/subcategory');
