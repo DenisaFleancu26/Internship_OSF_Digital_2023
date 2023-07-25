9065a57c77d2a7d5564ac08988fd8685fe3f15bc
Denisaa <101568110+DenisaFleancu26@users.noreply.github.com>
2023-07-25 22:55:18 +0300

    Registration functionality

--- routes/registration.js
+++ routes/registration.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
 router.get('/registration', (req, res) => {
   res.render('registrationPage', { 
@@ -10,5 +12,40 @@ router.get('/registration', (req, res) => {
   });
 });
 
+router.post('/registration', async (req, res) => {
+    
+    const { email, password, confirm_password } = req.body;
+
+    if(password !== confirm_password){
+        return res.json({ status: 'error', error: 'Passwords do not match!'});
+    }
+    if(!email){
+        return res.json({ status: 'error', error: 'Invalid Email!'});
+    }
+    if(!password){
+        return res.json({ status: 'error', error: 'Invalid Password'});
+    }
+    if(password.length < 6){
+        return res.json({status:"error", error:'The Password must be at least 6 characters'});
+    }
+    
+    const userPassword = await bcrypt.hash(password, 10);
+
+        try{
+            const user = await User.create({
+                email: email,
+                password: userPassword,
+            });
+            console.log('User created successfully', user);
+            res.json({ status: 'ok'});
+        }catch(error){
+            if(error.code === 11000){
+                return res.json({ status: 'error', error:'Email already in use!'})
+            }
+            throw error
+        }
+    
+  });
+ 
 
 module.exports = router;
